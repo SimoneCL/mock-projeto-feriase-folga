9f428069d84160aa1fc83bd2e8c2dc66965b6564
Simone Cristine Langer <simone.cristine>
2022-10-14 15:04:28 -0300

    commit do projeto

---
+++ index.js
@@ -0,0 +1,11 @@
+const mockServer = require('mock-totvs-builder');
+
+// Inicializa o Mock
+const server = mockServer.startMock(__dirname);
+
+// Carrega o Server
+if (server) {
+    server.listen(3000, () => { console.log('Mock no AR, Porta: 3000'); });
+} else {
+    console.log('Não foi possível carregar o Mock, verifique os arquivos de configuração (data) !');
+}
